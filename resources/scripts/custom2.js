124603195037eebce378e87b722618313a10cd5d
mikaspell <work@melkee.ru>
2017-10-16 02:29:35 +0300

    filter works now

---
+++ resources/scripts/custom2.js
@@ -0,0 +1,28 @@
+// Created by MikaSpell on 16.01.17.
+
+console.info('Пиши код красиво!', 'work@melkee.ru', '2017');
+
+document.addEventListener('click', function (e) {
+  var target = e.target;
+
+  if(target.tagName !== 'TD') return;
+  location.href = target.parentNode.getAttribute('data-url');
+});
+
+document.addEventListener('change', function (e) {
+  var target = e.target;
+
+  if(target.name !== 'types') return;
+
+  var rows = document.querySelectorAll('.all-tag-table tr'),
+    type;
+
+  rows.forEach(function (row) {
+    type = row.getAttribute('data-category');
+    if(type === target.value && !target.checked) {
+      row.style.display = 'none'
+    } else {
+      row.style.display = '';
+    }
+  });
+});
